6a8ce6f0942b53e47e304fc0dcb681b9b86f68da
Egor Kondratev <kondratevegor04@gmail.com>
2025-04-22 17:39:35 +0300

    chore: install main deps, setup infra

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from '@/styles/App.module.scss';
+
+const App = () => {
+  return (
+    <div className={styles.root}>
+      <h1 className={styles.heading}>Crypto Exchange</h1>
+    </div>
+  );
+};
+
+export { App };
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { configure } from 'mobx';
+import { App } from "@/App";
+import "@/styles/index.scss";
+
+// initial configuration for mobx
+setTimeout(() => {
+  configure({
+    enforceActions: 'never',
+    isolateGlobalState: true,
+    reactionScheduler: f => setTimeout(f),
+  });
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <p></p>
+    <App />
   </StrictMode>,
 );
