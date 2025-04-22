6486fe9ec4972ebd90cf22d1b748a96e05353291
Egor Kondratev <kondratevegor04@gmail.com>
2025-04-22 18:07:00 +0300

    feat: basic App styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import styles from '@/styles/App.module.scss';
+import { ExchangeForm } from "@/components";
 
 const App = () => {
   return (
     <div className={styles.root}>
       <h1 className={styles.heading}>Crypto Exchange</h1>
+
+      <ExchangeForm />
     </div>
   );
 };
---
+++ src/components/ExchangeForm/ExchangeForm.tsx
@@ -0,0 +1,11 @@
+import styles from './ExchangeForm.module.scss';
+
+const ExchangeForm = () => {
+  return (
+    <div className={styles.root}>
+      
+    </div>
+  );
+};
+
+export { ExchangeForm };
